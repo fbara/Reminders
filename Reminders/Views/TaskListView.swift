9959b81328815e2cfd3ed4f1f08eada84b4cc872
Frank S. Bara <fbara1@yahoo.com>
2020-05-23 19:03:58 -0500

    Completed #1.

--- Reminders/Views/TaskListView.swift
+++ Reminders/Views/TaskListView.swift
@@ -12,13 +12,23 @@ struct TaskListView: View {
     @ObservedObject var taskListViewModel = TaskListViewModel()
     let tasks = testDataTasks
     
+    @State private var presentAddNewItem = false
+    
     var body: some View {
         NavigationView {
             VStack(alignment: .leading) {
-                List(taskListViewModel.taskCellViewModels) { taskCellViewModel in
-                    TaskCell(taskCellViewModel: taskCellViewModel)
+                List {
+                    ForEach(taskListViewModel.taskCellViewModels) { taskCellViewModel in
+                        TaskCell(taskCellViewModel: taskCellViewModel)
+                    }
+                    if presentAddNewItem {
+                        TaskCell(taskCellViewModel: TaskCellViewModel(task: Task(title: "", completed: false))) { task in
+                            self.taskListViewModel.addTask(task: task)
+                            self.presentAddNewItem.toggle()
+                        }
+                    }
                 }
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Button(action: { self.presentAddNewItem.toggle() }) {
                     HStack {
                         Image(systemName: "plus.circle.fill")
                             .resizable()
@@ -42,12 +52,19 @@ struct ContentView_Previews: PreviewProvider {
 struct TaskCell: View {
     @ObservedObject var taskCellViewModel: TaskCellViewModel
     
+    var onCommit: (Task) -> (Void) = { _ in }
+    
     var body: some View {
         HStack {
             Image(systemName: taskCellViewModel.task.completed ? "checkmark.circle.fill" : "circle")
                 .resizable()
                 .frame(width: 20, height: 20)
-            Text(taskCellViewModel.task.title)
+                .onTapGesture {
+                    self.taskCellViewModel.task.completed.toggle()
+            }
+            TextField("Enter the task title", text: $taskCellViewModel.task.title, onCommit: {
+                self.onCommit(self.taskCellViewModel.task)
+            })
         }
     }
 }
